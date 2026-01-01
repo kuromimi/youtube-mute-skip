faf8a4f9980a69c757f5eb701a46a610befc3a84
kuromimi <31605860+kuromimi@users.noreply.github.com>
2026-01-01 20:27:57 +0900

    renew files

--- src/constants/actions.ts
+++
@@ -1,3 +0,0 @@
-export const CONFIGURE_CHANNEL = "CONFIGURE_CHANNEL" as const;
-export const GO_PREF_HOME = "GO_PREF_HOME" as const;
-export const VERIFY_SUBSCRIPTION = "VERIFY_SUBSCRIPTION" as const;
--- src/utils/datetime.ts
+++
@@ -1,6 +0,0 @@
-export function addMilliseconds(date: Date, offset: number): Date {
-  const clonedDate = new Date();
-  clonedDate.setTime(date.getTime() + offset);
-
-  return clonedDate;
-}
